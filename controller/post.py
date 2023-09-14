425ce743adf01455d909feb4b7f37a49b2929e31
Jonathan F <farinloyejonathan@gmail.com>
2023-09-14 14:25:38 +0100

    add caching on posts retrieval

--- controller/post.py
+++ controller/post.py
@@ -1,8 +1,10 @@
 from sqlalchemy.orm import Session
 from models.post import PostModel
 from models.sql_models import Post, User
+from cachetools import TTLCache, cached
 
 
+@cached(cache=TTLCache(maxsize=10, ttl=60 * 5))
 def get_users_db_posts(user: User, session: Session):
     posts = session.query(Post).filter_by(owner_id=user.id).all()
     return posts
